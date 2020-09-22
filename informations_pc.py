24a15472c62dd42bd487246558ab123685f9131c
evertoont <evertoontcarmo@gmail.com>
2020-09-22 17:26:07 -0300

    Create function main and menu

---
+++ informations_pc.py
@@ -0,0 +1,25 @@
+import platform
+
+def main():
+    option = int(input('''
+    [1] System informations
+    [2] Memory informations
+    [3] CPU informations
+    [4] GPU informations
+
+    Choose an option: '''))
+
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        pass
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    main()
