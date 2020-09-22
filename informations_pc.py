67b7041b1c4733cd0b60254e03ecde17de552fc1
evertoont <evertoontcarmo@gmail.com>
2020-09-22 17:26:55 -0300

    Create function system_informations

--- informations_pc.py
+++ informations_pc.py
@@ -1,5 +1,22 @@
 import platform
 
+def system_informations():
+    print("="*10, "System informations", "="*10)
+    info = platform.uname()
+
+    print(f'''
+    System: {info.system}
+    Computer name: {info.node}
+    Release: {info.release}
+    Version: {info.version}
+    Machine: {info.machine}
+    Processor: {info.processor}
+    
+    ''')
+
+    input("Press ENTER to return")
+    main()
+
 def main():
     option = int(input('''
     [1] System informations
@@ -10,7 +27,7 @@ def main():
     Choose an option: '''))
 
     if option == 1:
-        pass
+        system_informations()
     elif option == 2:
         pass
     elif option == 3:
